aab0f5338682739e986dccb57ca729583ae05635
Dongjun <dongjun@Dongjunui-MacBookPro.local>
2019-10-16 23:40:52 +0900

    activity and xml init

---
+++ app/SafeTicket/app/src/main/java/com/example/safeticket/TicketDialog.java
@@ -0,0 +1,13 @@
+package com.example.safeticket;
+
+import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public class TicketDialog extends Dialog {
+
+    public TicketDialog(@NonNull Context context) {
+        super(context);
+    }
+}
