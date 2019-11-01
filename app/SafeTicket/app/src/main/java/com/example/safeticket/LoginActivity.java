0446f435d555307ab01e212e9a3ce5b8239ff1d3
anstjaos <vv_eros@naver.com>
2019-11-01 17:17:23 +0900

    Update View Connection

--- app/SafeTicket/app/src/main/java/com/example/safeticket/LoginActivity.java
+++ app/SafeTicket/app/src/main/java/com/example/safeticket/LoginActivity.java
@@ -2,13 +2,45 @@ package com.example.safeticket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView findIdPwdText = (TextView) findViewById(R.id.findIdPwdText);
+        findIdPwdText.setOnClickListener(this);
+
+        Button signUpButton = (Button) findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(this);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), FindActivity.class);
+        switch (v.getId()){
+            case R.id.findIdPwdText:
+                break;
+            case R.id.signUpButton:
+                intent.setClass(getApplicationContext(), SignUpActivity.class);
+                break;
+            case R.id.loginButton:
+                intent.setClass(getApplicationContext(), MainActivity.class);
+                break;
+        }
+        startActivity(intent);
+        finish();
     }
 }
