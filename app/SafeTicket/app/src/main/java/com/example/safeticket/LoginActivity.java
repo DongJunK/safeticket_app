057ccc540f95df1ea89aac8f938cbabbeeb652ed
anstjaos <vv_eros@naver.com>
2019-11-02 17:34:28 +0900

    Develop Kakao Login SDK

--- app/SafeTicket/app/src/main/java/com/example/safeticket/LoginActivity.java
+++ app/SafeTicket/app/src/main/java/com/example/safeticket/LoginActivity.java
@@ -3,14 +3,36 @@ package com.example.safeticket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.kakao.auth.ErrorCode;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
 
 import org.w3c.dom.Text;
 
+import java.security.MessageDigest;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private SessionCallback callback;      //콜백 선언
+    //유저프로필
+    String token = "";
+    String name = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +45,44 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         Button signUpButton = (Button) findViewById(R.id.signUpButton);
         signUpButton.setOnClickListener(this);
 
-        Button loginButton = (Button) findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(this);
+        //Button loginButton = (Button) findViewById(R.id.loginButton);
+        //loginButton.setOnClickListener(this);
+
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+        Session.getCurrentSession().checkAndImplicitOpen();
+
+        getAppKeyHash();
+    }
+
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(callback);
     }
 
     @Override
@@ -36,11 +94,34 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.signUpButton:
                 intent.setClass(getApplicationContext(), SignUpActivity.class);
                 break;
-            case R.id.loginButton:
-                intent.setClass(getApplicationContext(), MainActivity.class);
-                break;
+//            case R.id.loginButton:
+//                intent.setClass(getApplicationContext(), MainActivity.class);
+//                break;
         }
         startActivity(intent);
         finish();
     }
+
+    private class SessionCallback implements ISessionCallback {
+        @Override
+        public void onSessionOpened() {
+            redirectSignupActivity();  // 세션 연결성공 시 redirectSignupActivity() 호출
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if (exception != null) {
+                Logger.e(exception);
+            }
+            setContentView(R.layout.activity_login); // 세션 연결이 실패했을때
+        }
+        // 로그인화면을 다시 불러옴
+    }
+
+    protected void redirectSignupActivity() {
+        final Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
